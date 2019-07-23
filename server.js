5528b2b719f6144f5d5b3e7c13f5d7396b94e1b1
mak20th <mak20th@gmail.com>
2019-07-22 23:35:18 -0500

    fina

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
-
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const validTypes = require('./types');
-const POKEDEX = require('./pokedex.json');//As JSON is a subset of JavaScript, we can require this file
-// // directly into our application and use it like a JavaScript object.
 const cors = require('cors');//npm i cors
 const helmet = require('helmet');//npm i helmet
 
+const POKEDEX = require('./pokedex.json');//As JSON is a subset of JavaScript, we can require this file
+// directly into our application and use it like a JavaScript object.
 
 const app = express();
 
+//app.use(morgan('dev'));
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganSetting));
-app.use(helmet());
+
+app.use(helmet()); //Make sure to place helmet before cors in the pipeline.
 app.use(cors());
 
-//~~~~~~~~COMPOSING the VALIDATION MIDDLEWARE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-app.use(function validateBearerToken(req, res, next) {
-  if (!req.get('Authorization')) {
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  const bearerToken = req.get('Authorization').split(' ')[1];
-  const apiToken = process.env.API_TOKEN;
-
-  if (bearerToken !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  next(); //callback at the end of the middleware to move to the next middleware in the pipeline. 
-  //   If we were to not invoke this next callback, the request would hang and we wouldn't see a response until there was a timeout.
-});
 
 //~~~~~function to get the types of pokemon~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.get('/types', function handleGetTypes(req, res) {
@@ -56,9 +43,23 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
   
   res.json(response);
 });
-////~~~~~~~~COMPOSING the VALIDATION MIDDLEWARE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//~~~~~~~~COMPOSING the VALIDATION MIDDLEWARE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  
+  // move to the next middleware
+  next(); //callback at the end of the middleware to move to the next middleware in the pipeline. 
+  //If we were to not invoke this next callback, the request would hang and we wouldn't see a response until there was a timeout.
+});
+  
 //hide sensitive error messages using below middleware . should be the LAST middleware in pipeline
-// // 4 parameters in middleware, express knows to treat this as error handler
+// 4 parameters in middleware, express knows to treat this as error handler
 app.use((error, req, res, next) => {
   let response;
   if (process.env.NODE_ENV === 'production') {
@@ -72,6 +73,5 @@ app.use((error, req, res, next) => {
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => { // code runs the app.listen method to connect it to a port number on the host machine
-  (`Server listening at http://localhost:${PORT}`
-  );
+  //console.log(`Server listening at http://localhost:${PORT}`);
 });
